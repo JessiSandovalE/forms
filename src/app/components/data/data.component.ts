ee93b1867b81f39883e4e82f2009529b63c50f2a
jessi <jessi.sandoval93@gmail.com>
2020-01-12 18:34:07 -0500

    Reactive Forms and Template forms

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormControl, Validators, FormArray}  from '@angular/forms';
 
 @Component({
   selector: 'app-data',
@@ -7,7 +8,86 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataComponent implements OnInit {
 
-  constructor() { }
+  forma: FormGroup;
+  usuario:Object = {
+    nombrecompleto: {
+      nombre:"Jessica",
+      apellido:"Sandoval"
+    },
+    correo: "jessi@gmail..com",
+  /*   pasatiempos:["Correr","Dormir","Comer"] */
+  }
+  constructor() { 
+
+    console.log(this.usuario);
+    this.forma = new FormGroup({
+
+      'nombrecompleto': new FormGroup({
+        'nombre': new FormControl('',  [
+          Validators.required,
+          Validators.minLength(3)
+        ] ),
+        'apellido': new FormControl('',[ 
+          Validators.required,
+          this.noSandoval
+        ])
+      })
+      ,
+      'correo': new FormControl('',   [
+        Validators.required, 
+        Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")
+      ]),
+
+      'pasatiempos': new FormArray([
+        new FormControl('',Validators.required)
+      ]),
+      'pasword1': new FormControl('', Validators.required),
+      'pasword2': new FormControl('', Validators.required),
+    })
+
+   /*  this.forma.setValue( this.usuario ); */
+  }
+
+  
+
+  agregarPasatiempo(){
+    (<FormArray>this.forma.controls['pasatiempos']).push(
+      new FormControl('',Validators.required)
+    )
+  }
+
+
+  noSandoval( control:FormControl ):{ [s:string]:boolean }{
+      if (control.value === "Sandoval"){
+        return{
+          noherrrera:true
+        }
+      }
+      return null
+  }
+
+  noIgual( control:FormControl ):{ [s:string]:boolean }{
+    if (control.value === this.forma.controls['password1'].value){
+      return{
+        noigual:true
+      }
+    }
+    return null
+}
+
+  guardarCambios(){
+    console.log(this.forma);
+    this.forma.reset({
+      nombrecompleto:{
+        nombre:"",
+        apellido:""
+      },
+      correo:"",
+      pasatiempo:""
+    })
+  }
+
+  
 
   ngOnInit() {
   }
